fafab7235a4e704997f5c9ef7a6f34b9512f6426
Jemma Touma <jemma.touma@visuellplanering.se>
2022-12-26 15:36:07 +0100

    Day 1 complete

--- src/main/java/year2015/Day1.java
+++ src/main/java/year2015/Day1.java
@@ -2,28 +2,22 @@ package year2015;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Day1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Day 1 Not Quite Lisp");
 
         int initialFloor = 0;
         int basementPosition = 0;
-        String input = "";
-        ClassLoader classLoader = Day1.class.getClassLoader();
 
-        File Day1Input = new File(classLoader.getResource("Day1input.txt").getFile());
-        try {
-            Scanner myReader = new Scanner(Day1Input);
-            while (myReader.hasNext()){
-               input += myReader.next();
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        String input = getPuzzleInput();
 
         char[] inputArray = input.toCharArray();
         for (int i = 0;i<inputArray.length;i++) {
@@ -40,5 +34,12 @@ public class Day1 {
         System.out.println("Santa is on floor "+initialFloor);
         System.out.println("Basement position is " + basementPosition);
 
+    }
+
+    private static String getPuzzleInput() throws IOException {
+        String puzzleInput = "src/main/resources/Day1input.txt";
+        File puzzleDirectory = new File(puzzleInput);
+        Path filePath = Path.of(puzzleDirectory.getAbsolutePath());
+        return Files.readString(filePath);
     }
 }
